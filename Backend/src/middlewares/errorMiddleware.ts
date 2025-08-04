c54d77111ef9f03dfc2fa3a2383f40b3fb2cde43
Sugam Neupane <neupane.sugam.19.sn@gmail.com>
2025-08-04 15:52:13 +0545

    initial setup commit

--- Backend/src/middlewares/errorMiddleware.ts
+++ Backend/src/middlewares/errorMiddleware.ts
@@ -1,25 +1,19 @@
 import { Request, Response, NextFunction } from "express";
 
-const generalErrorHandler = (
-  err: any,
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  console.error("[Error]", err.stack || err);
+const generalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
+	console.error("[Error]", err.stack || err);
 
-  const statusCode = err.status || 500;
+	const statusCode = err.status || 500;
 
-  res.status(statusCode).json({
-    status: statusCode,
-    success: false,
-    message:
-      err.message || "Internal Server Error",
-    // Optional: send stack only in development
-    ...(process.env.NODE_ENV !== "production" && {
-      stack: err.stack,
-    }),
-  });
+	res.status(statusCode).json({
+		status: statusCode,
+		success: false,
+		message: err.message || "Internal Server Error",
+		// Optional: send stack only in development
+		...(process.env.NODE_ENV !== "production" && {
+			stack: err.stack,
+		}),
+	});
 };
 
 export default generalErrorHandler;
